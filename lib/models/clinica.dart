2881a504d767a41faadf81b08c3f925199628d4f
Paola Rodrigues <paolarodrigues158@gmail.com>
2021-05-23 15:45:27 -0300

    listar clinicas

--- lib/models/clinica.dart
+++ lib/models/clinica.dart
@@ -1,34 +1,55 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
-class Clinica {
-  final String nomeClinica;
-  final String inicioAtentimentos;
-  final String fimAtentimentos;
-  final String minutosAtentimentos;
-  final String valor;
+class Clinica with ChangeNotifier {
+  final String _uri =
+      'https://mobile-flutter-fb11c-default-rtdb.firebaseio.com/clinicas.json';
+  final _client = http.Client();
+  List<String> listaClinicas = [];
+  final String? id;
+  final String? nomeClinica;
+  final String? inicioAtentimentos;
+  final String? fimAtentimentos;
+  final String? minutosAtentimentos;
+  final String? valor;
   // final String diaSemana;
 
   Clinica(
-      {required this.nomeClinica,
-      required this.inicioAtentimentos,
-      required this.fimAtentimentos,
+      {this.id,
+      this.nomeClinica,
+      this.inicioAtentimentos,
+      this.fimAtentimentos,
       // required this.diaSemana,
-      required this.minutosAtentimentos,
-      required this.valor});
+      this.minutosAtentimentos,
+      this.valor});
 
-  // Future<void> addClinica(Clinica novaClinica) async {
-  //   // const url = '';
-  //   // var response = await http.get(url);
-  // }
+  Future<List<String>> carregaClinica() async {
+    try {
+      var clinica;
+      var response = await _client.get(Uri.parse(_uri));
+      Map<String, dynamic> data = json.decode(response.body);
+      data.forEach((idClinica, clinicaData) {
+        clinica = {
+          idClinica,
+          clinicaData['nomeClinica'],
+          clinicaData['inicioAtentimentos'],
+          clinicaData['fimAtentimentos'],
+          clinicaData['minutosAtentimentos'],
+          clinicaData['valor'],
+        };
+        listaClinicas.add(clinica.toString());
+      });
+      return listaClinicas;
+    } finally {
+      _client.close();
+    }
+  }
 
   Future<void> addClinica() async {
-    var client = http.Client();
     try {
-      var uriResponse = await client.post(
-          Uri.parse(
-              'https://mobile-flutter-fb11c-default-rtdb.firebaseio.com/clinicas.json'),
+      var uriResponse = await _client.post(Uri.parse(_uri),
           body: json.encode({
             'nomeClinica': 'teste',
             'inicioAtentimentos': this.inicioAtentimentos,
@@ -36,9 +57,8 @@ class Clinica {
             'minutosAtentimentos': this.minutosAtentimentos,
             'valor': this.valor
           }));
-      print(uriResponse);
     } finally {
-      client.close();
+      _client.close();
     }
   }
 }
--- lib/screens/clinica/clinicas_screen.dart
+++ lib/screens/clinica/clinicas_screen.dart
@@ -1,9 +1,23 @@
 import 'package:agenda/Utils/app_routs.dart';
+import 'package:agenda/models/clinica.dart';
 import 'package:agenda/models/home_page_icons.dart';
 import 'package:flutter/material.dart';
 
-class ClinicasScreen extends StatelessWidget {
-  const ClinicasScreen({Key? key}) : super(key: key);
+class ClinicasScreen extends StatefulWidget {
+  @override
+  _ClinicasScreen createState() => _ClinicasScreen();
+}
+
+// ignore: must_be_immutable
+class _ClinicasScreen extends State<ClinicasScreen> {
+  List<String> listaClinicas = [];
+  late Future<List<String>> vai;
+
+  @override
+  void initState() {
+    vai = Clinica().carregaClinica();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +25,7 @@ class ClinicasScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as HomePageIcons;
 
     final mediaQuery = MediaQuery.of(context);
-    
+
     final PreferredSizeWidget appBar = AppBar(
       title: Text(homePageIcons.title),
     );
@@ -19,66 +33,78 @@ class ClinicasScreen extends StatelessWidget {
     final availabelHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
         mediaQuery.padding.top;
+    return Container(
+      child: FutureBuilder(
+        future: vai,
+        builder: (ctx, snapshot) {
+          if (!snapshot.hasData)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          listaClinicas = snapshot.data.toString().split("}, {");
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              homePageIcons.title,
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              height: availabelHeight * 0.9,
-              width: double.infinity,
-              padding: EdgeInsets.all(10),
-              child: ListView.builder(
-                itemCount: 12,
-                itemBuilder: (ctx, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          width: 300,
-                          padding: EdgeInsets.all(15),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Text(
-                            'Nome da Clínica',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
+          return Scaffold(
+            appBar: AppBar(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    homePageIcons.title,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
                     ),
-                  );
-                },
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () =>
-            Navigator.of(context).pushNamed(AppRoute.CLINICAS_CADASTRO),
-        backgroundColor: Colors.blue.shade500,
-        child: Icon(
-          Icons.add,
-          size: 35,
-        ),
+            body: Center(
+              child: Column(
+                children: [
+                  Container(
+                    height: availabelHeight * 0.9,
+                    width: double.infinity,
+                    padding: EdgeInsets.all(10),
+                    child: ListView.builder(
+                      itemCount: listaClinicas.length,
+                      itemBuilder: (value, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Container(
+                                width: 300,
+                                padding: EdgeInsets.all(15),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Text(
+                                  listaClinicas[index].split(",")[1],
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () =>
+                  Navigator.of(context).pushNamed(AppRoute.CLINICAS_CADASTRO),
+              backgroundColor: Colors.blue.shade500,
+              child: Icon(
+                Icons.add,
+                size: 35,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
